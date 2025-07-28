c684e03a5f8cb355444395aa228c09b704d4d5a8
bahaeddineDZ <bezzinabb@gmail.com>
2025-07-28 10:37:04 +0100

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-# تحميل بيانات الاعتماد (ضع اسم ملفك المفتاح هنا)
-cred = credentials.Certificate("dungeon-bot--nova-firebase-adminsdk-fbsvc-f1014530c2.json")
+cred_json = os.getenv("FIREBASE_CREDENTIAL")
+if not cred_json:
+    raise Exception("❌ لم يتم العثور على متغير البيئة FIREBASE_CREDENTIAL")
 
-# تهيئة التطبيق (مرة واحدة فقط)
+cred_dict = json.loads(cred_json)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
-# إنشاء مرجع إلى قاعدة بيانات Firestore
 db = firestore.client()
